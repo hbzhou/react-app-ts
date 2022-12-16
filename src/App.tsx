48cb11277d3e18d4326a1af734765e7506384182
Jeremy Zhou <jeremygilbert@126.com>
2022-12-16 23:42:41 +0800

    feat:NO BS TS #20 & #21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useReducer, useRef, useState } from "react";
+import "./App.css";
+
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
+
+const Box: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div
+    style={{
+      padding: "1rem",
+      fontWeight: "bold",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const List: React.FC<{
+  items: Array<string>;
+  onClick?: (item: string) => void;
+}> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => {
+      return (
+        <li key={index} onClick={() => onClick?.(item)}>
+          {item}
+        </li>
+      );
+    })}
+  </ul>
+);
+
+type Payload = {
+  id: number;
+  text: string;
+};
+
+type Todo = {
+  id: number;
+  done: boolean;
+  text: string;
+};
+
+type ActionType = { type: "ADD"; text: string } | { type: "REMOVE"; id: number };
 
 function App() {
+  const onItemClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    setPayload({
+      id: 1,
+      text: "This is the payload",
+    });
+  }, []);
+
+  const [todos, dispatch] = useReducer(
+    (state: Todo[], action: ActionType) => {
+      switch (action.type) {
+        case "ADD":
+          return [
+            ...state,
+            {
+              id: state.length + 1,
+              text: action.text,
+              done: false,
+            },
+          ];
+        case "REMOVE":
+          return state.filter(({ id }) => id !== action.id);
+
+        default:
+          throw new Error();
+      }
+    },
+    [
+      {
+        id: 1,
+        text: "shopping",
+        done: true,
+      },
+    ]
+  );
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current && newTodoRef.current.value) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title='Introduction' />
+      <Box>Hello there</Box>
+      <List items={["one", "two", "three"]} onClick={onItemClick}></List>
+      <Box>{JSON.stringify(payload)}</Box>
+      <Heading title='Todos' />
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            {todo.text}
+            <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>Remove</button>
+          </div>
+        );
+      })}
+      <div>
+        <input type='text' ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+//

be0464a2fcede0d3678dacfa090fcaf5ea49aa00
Jeremy Zhou <jeremygilbert@126.com>
2022-12-17 20:29:52 +0800

    feat: add type for UL component

--- src/components/UL.tsx
+++ src/components/UL.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function UL<T>({ items, render }: { items: T[]; render: (item: T) => React.ReactNode }) {
+function UL<T>({
+  items,
+  render,
+}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement> & {
+  items: T[];
+  render: (item: T) => React.ReactNode;
+}) {
   return (
     <ul>
       {items.map((item, index) => {
